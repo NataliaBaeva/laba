f0fb15876c3c090715d95dad8b76b527d51d6b89
Natalia <natalia.baieva@gmail.com>
2019-11-16 15:02:20 +0300

    Очередное изменение

--- core/EquationInterface.php
+++ core/EquationInterface.php
@@ -1,1 +1,7 @@
-<?phpnamespace core;interface EquationInterface { 	public function solve($a, $b, $c); }
+<?php
+namespace core;
+interface EquationInterface {
+	public function solve($a, $b, $c)
+}
+?>
+
