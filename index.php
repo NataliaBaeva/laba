4a0096541b14848ca428741c9e8c791a2fcfaf60
Natalia <natalia.baieva@gmail.com>
2019-11-16 13:28:27 +0300

    Лабораторная 2

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-                                  <?php    
+                                  <?php    /*
 class A{
 }
 class B extends A {
@@ -22,8 +22,68 @@ $ob5 = new B($ob4);
 echo '<pre>';
 var_dump($ob5);
 echo '</pre>';
-                             
-     
+                             */     
+
+
+class Linear {
+    private $x;
+    public function linear($a,$b){
+    	if($a==0){
+    	$this->x = false;
+    	}	
+        $this->x = (-$b/$a);
+        return $this->x;
+    }
+
+}
+        		
+class Square extends linear {
+	private $x1;
+	private $discr;
+
+	protected function discriminant($a,$b,$c){
+		return (($b*$b) - (4 * $a * $c));
+	}
+
+	public function square($a,$b,$c)
+	{
+		if($a==0)
+        {
+			$this->x1 = array($this->linear($b,$c));
+		}
+		else
+		{
+			$discr = $this->discriminant($a,$b,$c);
+			if ($discr < 0)
+			{
+				return $this->x1 = false;
+			}
+			elseif ($discr > 0)
+			{
+				$t1=((-$b + sqrt($discr)) / (2 * $a));
+				$t2=((-$b - sqrt($discr)) / (2 * $a));
+				return $this->x1= array($t1,$t2);
+			}
+		
+			else
+			{
+				return $this->x1 = array((-$b/2*$a));
+			}
+		}		
+	}
+}
+
+$ob1 = new A();
+$ob2 = new B($ob1);
+list($xx1,$xx2) = $ob2->square(1,4,1);
+echo $xx1."<br>".$xx2."<br>";
+
+
+
+
+
+
+
 
 ?>
 
